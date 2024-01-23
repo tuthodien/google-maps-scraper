9d93b8efec6ef26e5e25cde3867c7c3d9eb844c4
noreply <noreply@users.noreply.github.com>
2024-01-23 20:26:59 +0530

    fixes

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ def unicode_to_ascii(text):
     """
     Convert unicode text to ASCII, replacing special characters.
     """
+    if text is None:
+        return None
+
     # Replacing 'ë' with 'e' and return the ASCII text
     return unidecode(text).replace("ë", "e")
 
